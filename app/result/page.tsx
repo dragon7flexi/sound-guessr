9fd95e9d81c3a2bf3ebec2dfb1ed1b97b989282d
dragon7flexi <dragon7.flexi@gmail.com>
2024-10-31 18:55:16 +0900

    delete unnecessary codes.

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useSearchParams } from "next/navigation";
 import LinkButton from "../components/LinkButton";
@@ -8,20 +8,19 @@ export default function ResultContent() {
     const incorrectCnts = searchParams.get("incorrectCnts");
 
     // incorrectCntsをJSON.parseで配列に変換
-    const incorrectCountsArray = incorrectCnts ? JSON.parse(incorrectCnts) : [];
+    const incorrectCountsArray: number[] = incorrectCnts ? JSON.parse(incorrectCnts) : [];
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <h1 className="text-7xl">間違えた回数</h1>
             <ul>
-                {incorrectCountsArray.map((cnt: any, idx: any) => (
-                    <ul key={idx} className="text-3xl py-3">{idx + 1}問目: {cnt}</ul>
+                {incorrectCountsArray.map((cnt: number, idx: number) => (
+                    <li key={idx} className="text-3xl py-3">{idx + 1}問目: {cnt}</li>
                 ))}
             </ul>
 
             <LinkButton href="/">トップページへ戻る</LinkButton>
             <LinkButton href="/lesson">もう一度</LinkButton>
-
         </div>
     );
 }
